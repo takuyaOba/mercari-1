f9415afb888aeebec6d09c0863e3ad91efde4a55
nobu04 <ditch.mouse@gmail.com>
2019-06-12 15:00:21 +0900

    画像名称変更およびswiperの実装

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var swiper = new Swiper('main .swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      reverseDirection: false
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+}, false);
+
+
