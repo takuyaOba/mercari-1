490a988e9daa612578f9668927e302be779292c0
nobu04 <ditch.mouse@gmail.com>
2019-06-14 14:26:42 +0900

    prefectureのコメントアウト

--- app/models/Prefectures.rb
+++ app/models/Prefectures.rb
@@ -1,4 +1,4 @@
-class Prefectures < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-end
+# class Prefectures < ApplicationRecord
+#   extend ActiveHash::Associations::ActiveRecordExtensions
+#   belongs_to_active_hash :prefecture
+# end
--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,20 +1,20 @@
-class Prefecture < ActiveHash::Base
-  self.data = [
-      {id: 1, city: '北海道'}, {id: 2, city: '青森県'}, {id: 3, city: '岩手県'},
-      {id: 4, city: '宮城県'}, {id: 5, city: '秋田県'}, {id: 6, city: '山形県'},
-      {id: 7, city: '福島県'}, {id: 8, city: '茨城県'}, {id: 9, city: '栃木県'},
-      {id: 10, city: '群馬県'}, {id: 11, city: '埼玉県'}, {id: 12, city: '千葉県'},
-      {id: 13, city: '東京都'}, {id: 14, city: '神奈川県'}, {id: 15, city: '新潟県'},
-      {id: 16, city: '富山県'}, {id: 17, city: '石川県'}, {id: 18, city: '福井県'},
-      {id: 19, city: '山梨県'}, {id: 20, city: '長野県'}, {id: 21, city: '岐阜県'},
-      {id: 22, city: '静岡県'}, {id: 23, city: '愛知県'}, {id: 24, city: '三重県'},
-      {id: 25, : '滋賀県'}, {id: 26, city: '京都府'}, {id: 27, city: '大阪府'},
-      {id: 28, city: '兵庫県'}, {id: 29, city: '奈良県'}, {id: 30, city: '和歌山県'},
-      {id: 31, city: '鳥取県'}, {id: 32, city: '島根県'}, {id: 33, city: '岡山県'},
-      {id: 34, city: '広島県'}, {id: 35, city: '山口県'}, {id: 36, city: '徳島県'},
-      {id: 37, city: '香川県'}, {id: 38, city: '愛媛県'}, {id: 39, city: '高知県'},
-      {id: 40, city: '福岡県'}, {id: 41, city: '佐賀県'}, {id: 42, city: '長崎県'},
-      {id: 43, city: '熊本県'}, {id: 44, city: '大分県'}, {id: 45, city: '宮崎県'},
-      {id: 46, city: '鹿児島県'}, {id: 47, city: '沖縄県'}
-  ]
-end
+# class Prefecture < ActiveHash::Base
+#   self.data = [
+#       {id: 1, city: '北海道'}, {id: 2, city: '青森県'}, {id: 3, city: '岩手県'},
+#       {id: 4, city: '宮城県'}, {id: 5, city: '秋田県'}, {id: 6, city: '山形県'},
+#       {id: 7, city: '福島県'}, {id: 8, city: '茨城県'}, {id: 9, city: '栃木県'},
+#       {id: 10, city: '群馬県'}, {id: 11, city: '埼玉県'}, {id: 12, city: '千葉県'},
+#       {id: 13, city: '東京都'}, {id: 14, city: '神奈川県'}, {id: 15, city: '新潟県'},
+#       {id: 16, city: '富山県'}, {id: 17, city: '石川県'}, {id: 18, city: '福井県'},
+#       {id: 19, city: '山梨県'}, {id: 20, city: '長野県'}, {id: 21, city: '岐阜県'},
+#       {id: 22, city: '静岡県'}, {id: 23, city: '愛知県'}, {id: 24, city: '三重県'},
+#       {id: 25, : '滋賀県'}, {id: 26, city: '京都府'}, {id: 27, city: '大阪府'},
+#       {id: 28, city: '兵庫県'}, {id: 29, city: '奈良県'}, {id: 30, city: '和歌山県'},
+#       {id: 31, city: '鳥取県'}, {id: 32, city: '島根県'}, {id: 33, city: '岡山県'},
+#       {id: 34, city: '広島県'}, {id: 35, city: '山口県'}, {id: 36, city: '徳島県'},
+#       {id: 37, city: '香川県'}, {id: 38, city: '愛媛県'}, {id: 39, city: '高知県'},
+#       {id: 40, city: '福岡県'}, {id: 41, city: '佐賀県'}, {id: 42, city: '長崎県'},
+#       {id: 43, city: '熊本県'}, {id: 44, city: '大分県'}, {id: 45, city: '宮崎県'},
+#       {id: 46, city: '鹿児島県'}, {id: 47, city: '沖縄県'}
+#   ]
+# end
