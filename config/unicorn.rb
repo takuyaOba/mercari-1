e9f009f4c9828bda275113727a061bfcbd8c33a7
nobu04 <ditch.mouse@gmail.com>
2019-06-15 23:03:53 +0900

    直接のパスに修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,17 +8,17 @@ worker_processes 1
 working_directory "/var/www/mercari/current"
 
 #ポート番号を指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+listen "/var/www/mercari/shared/tmp/sockets/unicorn.sock"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+pid "/var/www/mercari/shared/tmp/pids/unicorn.pid"
 
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stderr_path "/var/www/mercari/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+stdout_path "/var/www/mercari/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
