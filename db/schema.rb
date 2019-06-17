0e9b253f6061b9ee24075e702bd042a18ba0bd72
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-06-17 19:29:35 +0900

    create collumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_062257) do
+ActiveRecord::Schema.define(version: 2019_06_17_100242) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 2019_06_17_062257) do
     t.bigint "third_category_id"
     t.bigint "brand_id"
     t.bigint "size_id"
+    t.string "condition", null: false
+    t.string "delivery_burden", null: false
+    t.string "delivery_way", null: false
+    t.string "prefecture", null: false
+    t.string "delivery_days", null: false
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["first_category_id"], name: "index_items_on_first_category_id"
     t.index ["second_category_id"], name: "index_items_on_second_category_id"
