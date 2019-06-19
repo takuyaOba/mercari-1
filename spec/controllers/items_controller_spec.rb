f0d203856a766f60a08085681478226ec4faeb25
yujiro7029 <kawa0320@outlook.jp>
2019-06-19 20:01:02 +0900

    テストコード記述の準備
    # 何をした？
    
    テストコード実装にあたりgemを導入
    必要なファイルとディレクトリを作成。
    
    # 実装するテスト
    - itemコントローラのindexアクションに関する記述。
    
    - itemモデル

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe ItemsController do
+  describe 'GET #index' do
+    it'ここにindexコントローラのテストコードをかく'do
+  end
+ 
+
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Item do
+  describe '#index' do
+    it "itemsモデルに関するテストを書く" do
+      
+    end
+  end
+end  
