6cc01382393368955e2fbe813cc43133d541fb59
yujiro7029 <kawa0320@outlook.jp>
2019-06-19 12:10:38 +0900

    add

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -4,6 +4,6 @@ class ItemImage < ApplicationRecord
 
 
   belongs_to :item, optional: true
-  mount_uploader :image, ImageUploader
+  
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_06_18_062500) do
-
 ActiveRecord::Schema.define(version: 2019_06_18_065805) do
-
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
