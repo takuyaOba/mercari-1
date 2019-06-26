b01164dbdb0455dea1ef52aae446b6fd9b484a7e
nobu04 <ditch.mouse@gmail.com>
2019-06-26 16:55:53 +0900

    destroyaction仮置き設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -64,6 +64,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+
     @item = Item.find(params[:id])
     if @item.user_id == current_user.id
       @item.destroy
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,6 +1,6 @@
 class ItemImage < ApplicationRecord
 
-  belongs_to :item,optional: true
+  belongs_to :item, optional: true
   mount_uploader :image, ImageUploader
 
 end
