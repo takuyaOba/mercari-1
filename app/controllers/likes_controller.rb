6dc15ed709d715af82231a1df49aace755ae06fe
yujiro7029 <kawa0320@outlook.jp>
2019-06-26 20:10:04 +0900

    current_userに変更
    仮置きで入れていた数字をcurrent_userに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,14 @@
 class LikesController < ApplicationController
   
   def create
-    #current_userの代わりに代替えのidを入れています
-    like = Like.new(user_id: 8,item_id: params[:item_id])
+    like = Like.new(user_id:current_user.id,item_id: params[:item_id])
     like.save
     redirect_to item_path(params[:item_id])
   end
 
-  def destroy
-    # current_userの代わりに8を入れています
-    like = Like.find_by(user_id:8,item_id: params[:item_id])
+  def destory
+    like = Like.find_by(user_id:current_user.id,item_id: params[:item_id])
     like.destroy
+    redirect_to item_path(params[:item_id])
   end
 end
