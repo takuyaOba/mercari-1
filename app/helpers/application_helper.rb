488eb637d435f5dbda145dc14d8a0b088cca251c
takuyaOba <letztbattalion001@gmail.com>
2019-06-27 15:37:24 +0900

    showへログインせず飛べるように、中でログインへ促す

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # items/showいいねの表示部分
 
   def like_judgment
-    if Like.find_by(user_id:current_user.id,item_id: @item.id)
+    if Like.find_by(user_id:current_user&.id,item_id: @item.id)
       link_to @item.likes.count,"/likes/#{@item.id}/create",method: :delete
     else 
       link_to @item.likes.count,"/likes/#{@item.id}/create",method: :post
@@ -12,7 +12,7 @@ module ApplicationHelper
 
   # items/show不適切な商品を報告
   def flag_list
-    if Flag.find_by(user_id:current_user.id,item_id: @item.id)
+    if Flag.find_by(user_id:current_user&.id,item_id: @item.id)
       p "報告済み"
     else
       link_to "不適切な商品を報告","/flags/#{@item.id}/new"
