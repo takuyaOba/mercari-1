f0361944b6b6aa6369a13e2171ec8e8991eff536
yujiro7029 <kawa0320@outlook.jp>
2019-06-27 13:31:03 +0900

    消し忘れ
    データーの消し忘れ

--- app/assets/javascripts/item-show.js
+++ app/assets/javascripts/item-show.js
@@ -1,7 +1,6 @@
 
 $(function() {
   $('.slide-box').on("mouseover",function(){
-    $(this).css('opacity', '0.5')
     var image = $(this).attr("src");
     $('#main-box').attr('src',image);
     $(this).css('opacity', '1.0')
