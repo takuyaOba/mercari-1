6f47d591cea243d85a665f75e666f70c41911742
takuyaOba <letztbattalion001@gmail.com>
2019-06-27 20:25:50 +0900

    showあると詳細入れなかった。削除忘れ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
 
    before_action :set_item,only:[:show]
-   before_action :authenticate_user!, only: [:show,:new]
+   before_action :authenticate_user!, only: [:new]
 
    rescue_from ActiveRecord::RecordInvalid do |exception|
     redirect_to :root, alert: 'エラーが発生しました'
