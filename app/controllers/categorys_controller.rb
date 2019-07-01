028a1b505bcea8e0aac8f461b00c74f0383e19bc
yujiro7029 <kawa0320@outlook.jp>
2019-07-01 20:27:22 +0900

    カテゴリー検索デザインの完成
    カテゴリ検索のデザインの完成

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -2,7 +2,13 @@ class CategorysController < ApplicationController
 
   def index
     @lists = FirstCategory.all
-   
+
+    @tops = ThirdCategory.genre(1)
+    @jackets = ThirdCategory.genre(2)
+    @pants = ThirdCategory.genre(3)
+    
+
+
   end
 
   def show
