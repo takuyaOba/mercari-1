b3d7ec672437ef24ed3ed6daa95fabac1a729cf1
yujiro7029 <kawa0320@outlook.jp>
2019-07-02 14:33:58 +0900

    必要ない記述の削除
    消し忘れ

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -3,9 +3,7 @@ class CategorysController < ApplicationController
   def index
     @lists = FirstCategory.all
 
-    @tops = ThirdCategory.genre(1)
-    @jackets = ThirdCategory.genre(2)
-    @pants = ThirdCategory.genre(3)
+   
     
 
 
