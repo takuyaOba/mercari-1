15feafdcbea3155156a5957cd30533380c5af135
yujiro7029 <kawa0320@outlook.jp>
2019-07-03 16:58:12 +0900

    一覧表示に機能に追加
    ブランドテーブルからデーターを取得し、投稿順に一覧表示する機能を追加

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,10 @@
 class Brand < ApplicationRecord
   has_many :items
   has_many :first_categories
+
+  #index（スコープ設定.brand）
+  scope :brand, ->(brand_name){where(name:(brand_name)).order(created_at: :DESC).limit(4)}
+
     # ,through: :brand_categories
   # has_many :brand_categories
 end
